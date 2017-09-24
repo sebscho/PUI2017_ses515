fbc93d30961d2a1838a3c6720cf2670994982570
Sarah Schoengold <sarah.schoengold@ted.com>
2017-09-24 16:39:13 -0400

    add script

---
+++ HW2_ses515/show_bus_location_ses515.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+
+import pylab as pl
+import os
+import json
+import pandas as pd
+import sys
+
+try:
+    import urllib2 as urllib
+except ImportError:
+    import urllib.request as urllib
+
+# %pylab inline
+pl.rc('font', size=15)
+
+key = sys.argv(1)
+line = sys.argv(2)
+
+if not len(sys.argv) == 3:
+    print ("Invalid number of arguments.")
+    sys.exit()
+
+url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=" + key + \
+   "a&VehicleMonitoringDetailLevel=calls&LineRef=" + line
+
+response = urllib.urlopen(url)
+data = response.read().decode("utf-8")
+data = json.loads(data)
+
+allbuses = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+
+bus_list = []
+for bus in range(len(allbuses)):
+    name = allbuses[bus]['MonitoredVehicleJourney']['PublishedLineName']
+    location = allbuses[bus]['MonitoredVehicleJourney']['VehicleLocation']
+    bus_list.append({"name": name, "location": location})
+    
+for i in range(len(allbuses)):
+    print ("Bus {i} is at {location}".format(i=i, location=location))   
+                
+
--- Oops/HW1_ses515/README.md
+++
@@ -1,7 +0,0 @@
-
-### HW1 - Finish the lab
-#### Lab 1 is complete
-See other repo "gittest_ses515" in my github profile, which includes:
-* repo made from terminal 
-* resolved conflict
-* forked/pull requested by neighbor
